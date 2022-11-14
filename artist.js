e04d8dff8cab38e6525bb0917e1d9a0822fe7bbe
Courtney Hampton <jaye0888@gmail.com>
2022-11-14 21:42:14 +0100

    adding jumbotron back in

---
+++ artist.js
@@ -0,0 +1,21 @@
+//the fetch
+
+let band = "queen";
+
+const getArtist = async () =>
+  await fetch(
+    `https://striveschool-api.herokuapp.com/api/deezer/artist/${band}`
+  )
+    .then((response) => response.json())
+    .then((artist) => {
+      changeBackground(artist);
+      console.log(artist);
+    });
+
+//change bg photo to Artist
+
+const changeBackground = (artist) => {
+  const backgroundPhoto = document.getElementById("mainPanel");
+
+  backgroundPhoto.style.backgroundImage = `url('${artist.picture_big}')`;
+};
