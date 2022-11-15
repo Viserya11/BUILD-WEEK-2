d4640d4033e5cc062ed5dad60f24c3db577a9473
Panna Szűcs <szucspann@gmail.com>
2022-11-15 16:29:59 +0100

    update

---
+++ home.js
@@ -0,0 +1,95 @@
+const card = (song) => `<div class="col-12 col-md-3 col-lg-2 card  m-2 text-center dark-cards">
+<img class="img-fluid" src="${song.album.cover_xl}" alt="img placeholder" />
+<p>
+  <a href="/album_page.html?albumId=${song.album.id}">${song.album.title}</a>
+  <br />
+  <a href="/artist_page.html">${song.artist.name}</a>
+</p>
+</div>`
+
+
+let defaultArtist = "queen"
+
+const search = async (artist = defaultArtist) => {
+
+const row = document.querySelector("#results .row")
+const h2 = document.querySelector("#results h2")
+
+
+const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + artist)
+const data = await response.json()
+const songs = data.data
+
+h2.innerText = artist
+
+row.innerHTML = ""
+
+songs.forEach(song => {
+
+row.innerHTML += card(song)
+})
+
+}
+
+
+
+const getSearch = event => {
+
+const query = document.getElementById("searchField").value
+
+if (query.length > 2) {
+search(query)
+
+}
+
+defaultArtist = event.target.value
+
+if (event.key === "Enter" && event.target.value.length > 3) {
+search()
+}
+
+
+}
+
+const christmasSongs = async () => {
+
+const goodMorningRow = document.querySelector('.goodMorning')
+const ul = document.getElementById('list')
+const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=santa'  )
+const data = await response.json()
+const songs = data.data
+console.log(songs)
+goodMorningRow.classList.add="text-center justifty-content-center"
+for(let i=0; i < songs.length-15; i++) {
+
+
+const song = songs[i]
+
+
+goodMorningRow.innerHTML += `<div class="col-5  col-lg-2  goodMorningDivs m-2 mb-5">
+<div class="img-div">
+ <img class="  " src="${song.album.cover_medium}" alt="img placeholder" height="80px"  />
+ </div>
+
+<div class="text-div">
+<a class=" a-gm ml-2" href="/album_page.html">${song.title_short}</a>
+</p></div>
+ 
+ 
+
+</div>
+`
+
+ul.innerHTML+= `<li class="li-list">${song.title_short}</li>`
+console.log(ul)
+}
+
+}
+
+
+christmasSongs()
+
+window.onload = () => {
+search()
+
+};
