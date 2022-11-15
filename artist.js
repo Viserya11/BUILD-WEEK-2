8c9ce85297a7ad55e68c68559931541e39c68eae
Courtney Hampton <jaye0888@gmail.com>
2022-11-15 14:55:14 +0100

    Populating relevant artist info

--- artist.js
+++ artist.js
@@ -10,6 +10,8 @@ const getArtist = async () =>
     .then((artist) => {
       changeBackground(artist);
       getId(artist);
+      getName(artist);
+      getNmOfFans(artist);
       console.log(artist);
     });
 
@@ -22,4 +24,17 @@ const changeBackground = (artist) => {
 };
 const getId = (artist) => {
   console.log(artist.id);
+};
+const getName = (artist) => {
+  const h1 = document.getElementsByTagName("h1");
+  h1.innerText = artist.name;
+};
+const getNmOfFans = (artist) => {
+  const numOfFans = document.querySelector(".lead");
+  const num = artist.nb_fan;
+
+  let obj1 = new Intl.NumberFormat("en-US");
+  let output1 = obj1.format(num);
+
+  numOfFans.innerText = `${output1} monthly listeners`;
 };
