03758c21029fca0f11a4060280ea943b86b50807
Panna Szűcs <szucspann@gmail.com>
2022-11-18 13:23:10 +0100

    styling

--- album.js
+++ album.js
@@ -81,6 +81,10 @@ const audio = document.querySelector("#audio")
     songduration.innerHTML = `<span class="duration">${fmtMSS(duration)}</span>`
     audio.src = preview
     audio.play()
+
+   const songtitle = document.querySelector('.songtitle').
+   songtitle.classList.add("greentext")
+    
 }
 
 
